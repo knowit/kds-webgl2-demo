859f063ccb08bd55e78dd11c80a223577cde7cc7
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-02-13 17:58:10 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Is the current build a development build
+const IS_DEV = (process.env.NODE_ENV === 'dev');
+
+const dirNode = 'node_modules';
+const dirApp = path.join(__dirname, '.');
+
+const appHtmlTitle = 'Demo WebGL';
+
+/**
+ * Webpack Configuration
+ */
+module.exports = {
+    devtool: 'eval',
+
+    output: {
+        pathinfo: true,
+        publicPath: '/',
+        filename: '[name].js'
+    },
+
+    devServer: {
+        host: '0.0.0.0'
+    },
+    
+    entry: {
+        bundle: path.join(dirApp, 'index')
+    },
+    resolve: {
+        modules: [
+            dirNode,
+            dirApp
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV: IS_DEV
+        }),
+
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.ejs'),
+            title: appHtmlTitle
+        })
+    ],
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    compact: true
+                }
+            }
+        ]
+    }
+};
