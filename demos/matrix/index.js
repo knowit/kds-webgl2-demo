0d1d7428d2c3c099ecbf876c054644f718e89403
Jens Kristoffer Retian Markussen <jens.markussen@knowit.no>
2019-04-12 08:07:09 +0200

    matrix multiplication gpu

--- demos/matrix/index.js
+++ demos/matrix/index.js
@@ -1,11 +1,24 @@
 
 import { context, dummyCanvas, computeKernel } from '../../lib/context'
+import { texture } from '../../lib/buffer'
+import { textureUniform } from '../../lib/program'
 
 const ctx = context(dummyCanvas({width: 1024, height: 1024}))
-
 const matrixKernel = computeKernel(ctx, {
     fragmentShader: require('./multiply.frag.glsl')
 })
 
-matrixKernel.execute(0.0)
+const matrix1 = texture(ctx, 1024, 1024, new Float32Array(
+    Array.from({length: 1024*1024}, () => 2.0)
+))
+
+const matrix2 = texture(ctx, 1024, 1024, new Float32Array(
+    Array.from({length: 1024*1024}, () => 4.0)
+))
+
+matrixKernel.execute(0.0, {
+    matrix1: textureUniform(0), 
+    matrix2: textureUniform(1)
+}, [matrix1, matrix2])
+
 console.log(matrixKernel.result.read())
--- demos/matrix/multiply.frag.glsl
+++ demos/matrix/multiply.frag.glsl
@@ -4,6 +4,18 @@ precision highp float;
 in vec2 uv;
 out float res;
 
+uniform sampler2D matrix1;
+uniform sampler2D matrix2;
+
 void main () {
-    res = uv.x;
+    float res0 = 0.0;
+    for (int i = 0; i < 1024; i++){
+        float k = float(i)/1024.0;
+
+        float cell1 = texture(matrix1, vec2(uv.x, k)).x;
+        float cell2 = texture(matrix2, vec2(k, uv.y)).x;
+
+        res0 += cell1*cell2;
+    }
+    res = res0;
 }
